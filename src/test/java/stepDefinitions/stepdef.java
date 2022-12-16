09dcc1f661eeda2de7014c0378f5f79d9f0b0871
sravanisaladi <sravanisaladi172@gmail.com>
2022-12-16 15:21:25 +0530

    added reports

--- src/test/java/stepDefinitions/stepdef.java
+++ src/test/java/stepDefinitions/stepdef.java
@@ -28,6 +28,7 @@ public class stepdef {
 	public void enter_source_and_destination_cities() throws InterruptedException {
 		b=new booking(driver);
 		b.sourceanddestination();
+		Reporter.log("entered source and destination cities");
 	   
 	}
 
@@ -35,22 +36,30 @@ public class stepdef {
 	public void enter_date() {
 		b.date();
 		b.returndate();
+		Reporter.log("entered date and return date");
 	  
 	}
 
 	@When("click on search button")
 	public void click_on_search_button() throws InterruptedException {
 		b.searchbtn();
+		Reporter.log("clicked on search");
 	   
 	}
 
 	@Then("Book tickets")
 	public void book_tickets() throws InterruptedException {
 		b.arrivaltime();
+		Reporter.log("selected arrival time");
 		b.depttime();
+		Reporter.log("selected departure time");
 		b.bustypes();
+		Reporter.log("selected the type of bus as sleeper");
 		b.seats();
+		Reporter.log("selected avaible seat");
 		b.passengerdetails();
+		Reporter.log("entered details");
+		Reporter.log("Successfully made payment");
 	    
 	}
 
